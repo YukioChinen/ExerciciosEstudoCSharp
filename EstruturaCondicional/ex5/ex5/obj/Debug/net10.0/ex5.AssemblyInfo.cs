74b7c31910ba9dbd94e9f116911ad2a8ac91327e
Enzo Yukio Chinen <yukiochinen25@gmail.com>
2026-04-17 15:20:40 -0300

    Exercícios sobre Estrutura While

--- EstruturaCondicional/ex5/ex5/obj/Debug/net10.0/ex5.AssemblyInfo.cs
+++ EstruturaCondicional/ex5/ex5/obj/Debug/net10.0/ex5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7057f71118b4b0b78f4245915827e8142849904f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669051fda2c1453a24e1cf33678733873c062ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EstruturaWhile/ex3/ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex1 {
+    class Program {
+        static void Main(string[] args) {
+            int alcool = 0, gasolina = 0, diesel = 0;
+            while (true) {
+                int n = int.Parse(Console.ReadLine());
+
+                if (n == 4) {
+                    Console.WriteLine("MUIO OBRIGADO");
+                    Console.WriteLine($"Alcool: {alcool} ");
+                    Console.WriteLine($"Gasolina: {gasolina}");
+                    Console.WriteLine($"Diesel: {diesel}");
+                    break;
+                }
+                if (n == 1) {
+                    alcool++;
+                }
+                else if (n == 2) {
+                    gasolina++;
+                }
+                else if (n == 3) {
+                    diesel++;
+                }
+            }
+
+        }
+    }
+}
